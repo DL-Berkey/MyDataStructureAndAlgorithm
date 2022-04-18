152f127afd77efbf02dc36b612a59ed14d696982
DL-Berkey <ruyria@naver.com>
2022-04-18 23:15:36 +0900

    add Graph/graph.js

---
+++ Graph/graph.js
@@ -0,0 +1,22 @@
+/*
+    이진트리
+    array로 표현하기
+    left = index * 2
+    light = index * 2 + 1
+    parent = floor(index / 2)
+*/
+
+// const tree = [
+//     //1
+//     9
+//     // 1*2 1*2+1
+//     3, 8,
+//     // 2*2, 2*2+1 3*2 3*2+1
+//     2, 5, undefined, 7,
+//     // 4*2 4*2+1 5*2 5*2+1
+//     undefined, undefined, undefined, 7
+// ]
+
+/*
+    이진트리를 연결 리스트로 구현
+*/
